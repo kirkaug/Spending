90af00f1b74b0d3278988edcd142351a46d1fac9
Kirk Augustine <kirk.augustine@carshield.com>
2023-11-06 08:15:59 -0600

    component generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ReportsComponent } from './reports/reports.component';
+import { BudgetPlanningComponent } from './budget-planning/budget-planning.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TransactionsComponent,
+    CategoriesComponent,
+    ReportsComponent,
+    BudgetPlanningComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/budget-planning/budget-planning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-planning',
+  templateUrl: './budget-planning.component.html',
+  styleUrls: ['./budget-planning.component.css']
+})
+export class BudgetPlanningComponent {
+
+}
